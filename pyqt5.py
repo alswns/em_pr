78d6e6f59f243f89bbfdb382803e471fdeb65818
alswns <49472611+alswns@users.noreply.github.com>
2019-08-13 13:23:21 +0900

    이름 넘겨주기만 하면 끝+디자인

--- pyqt5.py
+++ pyqt5.py
@@ -1,6 +1,7 @@
 import sys
 import PyQt5.QtWidgets as pq
 import PyQt5.QtGui as gui
+import PyQt5.QtCore as cr
 from pymongo import MongoClient
 #시작하기전에 cmd에서 mongod --dbpath D:\dev\mongoDB\data\db 입력
 
@@ -38,7 +39,7 @@ class LogInDialog(pq.QDialog):
             seat = list(map(int, seat))
             print(seat)
 
-
+        self.seat = seat
 
         self.name=str()
         self.all=[]
@@ -46,7 +47,7 @@ class LogInDialog(pq.QDialog):
         self.j=0
         self.id = None
         self.password = None
-        self.seat=seat
+
         self.setupUI()
     def setupUI(self):
 
@@ -57,7 +58,6 @@ class LogInDialog(pq.QDialog):
         label=pq.QLabel(info,self)
         label.move(100,10)
         number=1
-
         yplus = 0
 
         for i in range(5):
@@ -110,27 +110,52 @@ class Main_display(pq.QWidget):
         # posts = db.posts
         db = client['seat']
         self.collection = db['seat']
+        result = self.collection.find({'name': '제1강의실'})
+        for i in result:
+            seats = i['seat']
+        self.first_seat = list(map(int, seats))
+
+        result = self.collection.find({'name': '제2강의실'})
+        for i in result:
+            seats = i['seat']
+        self.second_seat= list(map(int, seats))
+        print(self.first_seat)
+        print(self.second_seat)
+
+        sing_in=login()
+        sing_in.exec_()
+        print(sing_in.toss)
+#        title=sing_in.
+
         self.initUI()
 
     def initUI(self):
         self.setWindowIcon(gui.QIcon('emotion_logo.jpg'))
         self.setWindowTitle('Emotion project')
         self.setGeometry(400,100,300,300)
+        first_all = len(self.first_seat)
+        first_remain = self.first_seat.count(1)
+        first_mesesge = str(first_remain) + '/' + str(first_all)
 
+        second_all = len(self.second_seat)
+        second_remain = self.second_seat.count(1)
+        second_mesesge = str(second_remain) + '/' + str(second_all)
 
         label=pq.QLabel('예약하실 교실을 선택하여 주십시오',self)
         label.move(50,60)
-        btn_first = pq.QPushButton('제1강의실',self)
-        btn_first.move(60, 110)
-        btn_first.resize(75,81)
-        btn_first.clicked.connect(self.pushButoon)
-        btn_second = pq.QPushButton('제2강의실', self)
-        btn_second.resize(75,81)
-        btn_second.clicked.connect(self.pushButoon)
+        self.btn_first = pq.QPushButton('제1강의실\n'+first_mesesge,self)
+        self.btn_first.move(60, 110)
+        self.btn_first.resize(75,81)
+        self.btn_first.clicked.connect(self.pushButoon)
+        self.btn_second = pq.QPushButton('제2강의실\n'+second_mesesge, self)
+        self.btn_second.resize(75,81)
+        self.btn_second.clicked.connect(self.pushButoon)
+        self.btn_second.move(170, 110)
+
         btn_format=pq.QPushButton('초기화',self)
         btn_format.move(100,280)
         btn_format.clicked.connect(self.format)
-        btn_second.move(170, 110)
+
         self.plus = pq.QLabel('예약하실 교실을 선택하여 주십시오',self)
         self.plus.move(50, 220)
 
@@ -141,15 +166,141 @@ class Main_display(pq.QWidget):
         self.collection.update({'name': '제2강의실'}, {'name': '제2강의실', 'seat': [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}, upsert=False)
     def pushButoon(self):
         inputs=self.sender().text()
-        self.collection.update({'id': 1}, {'id': 1, 'input': str(inputs)})
+        self.collection.update({'id': 1}, {'id': 1, 'input': str(inputs)[0:5]})
         display=LogInDialog()
-
-
         self.close()
         display.exec_()
+
+        result = self.collection.find({'name': '제1강의실'})
+        for i in result:
+            seats = i['seat']
+        self.first_seat = list(map(int, seats))
+
+        result = self.collection.find({'name': '제2강의실'})
+        for i in result:
+            seats = i['seat']
+        self.second_seat = list(map(int, seats))
+
+        first_all = len(self.first_seat)
+        first_remain = self.first_seat.count(1)
+        first_mesesge = '제1강의실\n'+str(first_remain) + '/' + str(first_all)
+
+        second_all = len(self.second_seat)
+        second_remain = self.second_seat.count(1)
+        second_mesesge = '제2강의실\n'+str(second_remain) + '/' + str(second_all)
+
+
         self.plus.move(70, 220)
         self.plus.setText(str(display.name)+'번 자리 예약 하셨습니다')
+        self.btn_first.setText(first_mesesge)
+        self.btn_second.setText(second_mesesge)
         self.show()
+
+class login(pq.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.resize(300,120)
+
+        client = MongoClient('localhost', 27017)
+        # localhost: ip주소
+        # 27017: port 번호
+        # db=client['seat']
+        # posts = db.posts
+        db = client['seat']
+        self.collection = db['member']
+        self.income_id=str()
+        self.toss=str()
+        self.initUI()
+
+
+    def initUI(self):
+        label=pq.QLabel("아이디와 학번을 입력하여 주십시오",self)
+        label.move(10,0)
+        label1 = pq.QLabel("학번:",self)
+        label1.move(10,30)
+        label2 = pq.QLabel("ID:",self)
+        label2.move(10, 60)
+        label1.alignment()
+        self.lineEdit1 = pq.QLineEdit(self)
+        self.lineEdit1.move(40,30)
+        self.lineEdit2 = pq.QLineEdit(self)
+        self.lineEdit2.move(40, 60)
+        self.pushButton1 = pq.QPushButton("로그인",self)
+        self.pushButton1.move(200,30)
+        self.pushButton1.clicked.connect(self.pushButtonClicked)
+        self.pushButton2 = pq.QPushButton("회원가입", self)
+        self.pushButton2.clicked.connect(self.pushButton2Clicked)
+        self.pushButton2.move(200, 60)
+        self.error=pq.QLabel('',self)
+        self.error.resize(171,16)
+        self.error.move(0,90)
+        self.error.setStyleSheet('color: red')
+    def pushButtonClicked(self):
+        self.class_num = self.lineEdit1.text()
+        self.id = self.lineEdit2.text()
+        result = self.collection.find({'class_num': self.class_num})
+        for i in result:
+            self.income_id = i['id']
+        if self.id == self.income_id:
+            self.close()
+        else:
+            self.error.setText('회원정보가 일치하지 않습니다')
+            return
+    def pushButton2Clicked(self):
+        self.close()
+        di=sing_up()
+        di.exec_()
+        self.toss=di.set_name
+
+
+class sing_up(pq.QDialog):
+    def __init__(self):
+        super().__init__()
+
+        client = MongoClient('localhost', 27017)
+        # localhost: ip주소
+        # 27017: port 번호
+        # db=client['seat']
+        # posts = db.posts
+        db = client['seat']
+        self.collection = db['member']
+        self.set_name=str()
+        self.resize(310,250)
+        self.initUI()
+    def initUI(self):
+        title=pq.QLabel('회원 정보를 입력하여주십시오',self)
+        title.move(20,20)
+        labe_number=pq.QLabel('학번:',self)
+        labe_number.move(20,70)
+        labe_id=pq.QLabel('ID:',self)
+        labe_id.move(20, 110)
+        labe_name=pq.QLabel('이름:',self)
+        labe_name.move(20, 150)
+
+        self.number=pq.QLineEdit(self)
+        self.number.move(70,70)
+        self.id = pq.QLineEdit(self)
+        self.id.move(70, 110)
+        self.name = pq.QLineEdit(self)
+        self.name.move(70, 150)
+        btn_ok=pq.QPushButton('확인',self)
+        btn_ok.move(220,220)
+        btn_ok.clicked.connect(self.pushButton)
+        self.error_text=pq.QLabel('',self)
+        self.error_text.resize(231,16)
+        self.error_text.move(10, 200)
+    def pushButton(self):
+        self.set_num=self.number.text()
+        set_id=self.id.text()
+        set_name=self.name.text()
+        informaition={'class_num':self.set_num,'id':set_id,'name':set_name}
+        data=self.collection.find_one({'class_num': self.set_num})
+
+        if data == None:
+            self.collection.insert(informaition)
+            self.close()
+        else:
+            self.error_text.setText('이미 있는 학번입니다.다시입력해주십시오')
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-a="111111101111111111001011110100"
-print(type(list(a)[1]))
+from pymongo import MongoClient
+client = MongoClient()
+
+client = MongoClient('localhost', 27017)
+db=client['seat']
+collection=db['member']
+set_num='10411'
+set_id='alswns0207'
+set_name='박민준'
+
+# result=collection.find_one({'class_num':set_num})
+# if result==None:
+#     print('있는정보입니다 다시입력해주십시오')
+# else:
+#     print('성공')
+if collection.find_one({'class_num': set_num}) == None:
+    print('성공')
+else:
+    print('있는정보입니다 다시입력해주십시오')
